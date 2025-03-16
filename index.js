6cb78fd29ead95b40a9a1bd30c018067816dbe1e
ElvisNdunguMbuthia <maishamagicast@gmail.com>
2025-03-16 16:13:57 +0300

    functions practise

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 // code your solution here
+function saturdayFun( activity ='roller-skate'){
+    return `This Saturday, I want to ${activity}!`
+}
+
+function mondayWork(activity ='go to the office'){
+    return `This Monday, I will ${activity}.`
+}
+
+function wrapAdjective(x){
+    if (x === '*'){
+        return function(y='unique'){
+            return `You are *${y}*!`
+        }
+    }
+    else if (x==='||'){
+        return function(y='amazing'){
+            return `You are ||${y}||!`
+        }
+    } 
+}
